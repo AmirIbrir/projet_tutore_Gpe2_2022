4ca0490235bf45798df583931565d39f5573e22a
Amir <ami.rouche@outlook.com>
2022-04-26 16:27:44 +0200

    database.php=>lier BDD à projet

---
+++ actions/database.php
@@ -0,0 +1,13 @@
+<!--Permet de lier database.ph à la BDD  -->
+<?php
+
+try{
+    //Variable qui nous permet d'acceder a la BDD 
+    //Nouvelle instance de la classe PDO, pour faire des requetes SQL-->
+    $bdd = new PDO('mysql:host=localhost;dbname=forum;charset=utf8;', 'root', '');
+}catch(Exception $e) { //variable 'e' qui stocke l'exception
+    die('Une erreur a été trouvée: ' . $e->getMessage()); //Dabord on plante (die) puis on affiche l'erreur
+}
+
+
+
