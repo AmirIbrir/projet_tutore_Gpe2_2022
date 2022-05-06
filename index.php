2d4844a3b5c93c07e628b16c8bfcb1ff732a37f8
Amir <ami.rouche@outlook.com>
2022-05-06 19:14:20 +0200

    ajout barre de recherche dans index.php

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php 
-
-require('actions/users/securityAction.php'); 
+    session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'includes/head.php'; ?>
 <body>
-<?php include 'includes/navbar.php'; ?>
+    <?php include 'includes/navbar.php'; ?>
 <br><br>
+
+    <!--Classe Bootstrap "container" qui permet de faire des marge dans la div-->
+    <div class="container">
+
+        <form method="GET">
+
+            <div class="form-group row">
+
+                <div class="col-8">
+                    <input type="search" name="search" class="form-control">
+                </div>
+
+                <div class="col-4"> 
+                    <button class="btn btn-success" type="submit">Rechercher</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
